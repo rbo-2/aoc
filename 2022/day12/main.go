f61ee418a18d8e2adb25da0fc98eeabc6dac7943
reza <sp1k3@proton.me>
2022-12-12 23:26:17 +0330

    refactor day12

--- 2022/day12/main.go
+++ 2022/day12/main.go
@@ -20,22 +20,20 @@ func inBound(grid [][]byte, y, x int) bool {
 	return len(grid) > y && y > -1 && x > -1 && len(grid[0]) > x
 }
 
-func stepsToDest(grid [][]byte, source []int, dest []int) int {
+func bfsPart1(grid [][]byte, source []int) [][]int {
 	dir := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 	if source[0] == -1 {
 		panic("couldnt find source pos")
 	}
-	grid[source[0]][source[1]] = 'a'
-	grid[dest[0]][dest[1]] = 'z'
 	visited := make([][]bool, len(grid))
 	for i := range visited {
 		visited[i] = make([]bool, len(grid[0]))
 	}
-	pathToSourceLen := make([][]int, len(grid))
-	for i := range pathToSourceLen {
-		pathToSourceLen[i] = make([]int, len(grid[0]))
+	stepsFromSource := make([][]int, len(grid))
+	for i := range stepsFromSource {
+		stepsFromSource[i] = make([]int, len(grid[0]))
 	}
-	pathToSourceLen[source[0]][source[1]] = 0
+	stepsFromSource[source[0]][source[1]] = 0
 	visited[source[0]][source[1]] = true
 	q := [][]int{}
 	q = append(q, []int{source[0], source[1]})
@@ -52,11 +50,48 @@ func stepsToDest(grid [][]byte, source []int, dest []int) int {
 			if cur+1 == adj || adj <= cur {
 				visited[newY][newX] = true
 				q = append(q, []int{newY, newX})
-				pathToSourceLen[newY][newX] += pathToSourceLen[curPos[0]][curPos[1]] + 1
+				stepsFromSource[newY][newX] += stepsFromSource[curPos[0]][curPos[1]] + 1
 			}
 		}
 	}
-	return pathToSourceLen[dest[0]][dest[1]]
+	return stepsFromSource
+}
+
+func bfsPart2(grid [][]byte, source []int) [][]int {
+	dir := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+	if source[0] == -1 {
+		panic("couldnt find source pos")
+	}
+	visited := make([][]bool, len(grid))
+	for i := range visited {
+		visited[i] = make([]bool, len(grid[0]))
+	}
+	stepsFromSource := make([][]int, len(grid))
+	for i := range stepsFromSource {
+		stepsFromSource[i] = make([]int, len(grid[0]))
+	}
+	stepsFromSource[source[0]][source[1]] = 0
+	visited[source[0]][source[1]] = true
+	q := [][]int{}
+	q = append(q, []int{source[0], source[1]})
+	for len(q) != 0 {
+		curPos := q[0]
+		q = q[1:]
+		for _, d := range dir {
+			newY, newX := curPos[0]+d[0], curPos[1]+d[1]
+			if !inBound(grid, newY, newX) || visited[newY][newX] {
+				continue
+			}
+			cur := grid[curPos[0]][curPos[1]]
+			adj := grid[newY][newX]
+			if cur-1 == adj || adj >= cur {
+				visited[newY][newX] = true
+				q = append(q, []int{newY, newX})
+				stepsFromSource[newY][newX] += stepsFromSource[curPos[0]][curPos[1]] + 1
+			}
+		}
+	}
+	return stepsFromSource
 }
 
 func part1(input []string) int {
@@ -70,8 +105,10 @@ func part1(input []string) int {
 	}
 	y, x := findPos(grid, 'S')
 	destY, destX := findPos(grid, 'E')
-	stepsFromStoE := stepsToDest(grid, []int{y, x}, []int{destY, destX})
-	return stepsFromStoE
+	grid[destY][destX] = 'z'
+	grid[y][x] = 'a'
+	steps := bfsPart1(grid, []int{y, x})
+	return steps[destY][destX]
 }
 
 func part2(input []string) int {
@@ -85,15 +122,14 @@ func part2(input []string) int {
 	}
 	y, x := findPos(grid, 'S')
 	destY, destX := findPos(grid, 'E')
-	stepsFromStoE := stepsToDest(grid, []int{y, x}, []int{destY, destX})
-	min := stepsFromStoE
-	for row := range grid {
-		for col := range grid[row] {
-			if grid[row][col] == 'a' {
-				steps := stepsToDest(grid, []int{row, col}, []int{destY, destX})
-				if steps < min && steps > 0 {
-					min = steps
-				}
+	grid[destY][destX] = 'z'
+	grid[y][x] = 'a'
+	steps := bfsPart2(grid, []int{destY, destX})
+	min := steps[y][x]
+	for i, row := range grid {
+		for j, e := range row {
+			if e == 'a' &&  steps[i][j]< min && steps[i][j] != 0 {
+				min = steps[i][j]
 			}
 		}
 	}
